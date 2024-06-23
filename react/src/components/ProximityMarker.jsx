dc7ddb9a286a43b19e7f7843c3dcf1ae543706a7
Steven Hutchison <hutchison.music@gmail.com>
2024-06-23 13:51:24 -0500

    Render viewed drop icon regardless of distance

--- react/src/components/ProximityMarker.jsx
+++ react/src/components/ProximityMarker.jsx
@@ -30,8 +30,10 @@ export default function ProximityMarker({drop}) {
     }
     if (drop.creatorInfo._id === profile._id) {
         return <NearDrop drop={drop} icon={myDrop} />
+    } else if (drop.viewedBy.includes(profile._id)) {
+        return <NearDrop drop={drop} icon={icons.drop} />
     } else if (distance <= 100) {
-        return  <NearDrop drop={drop} icon={drop.viewedBy.includes(profile._id) ? icons.drop : icons.newDrop}/>
+        return <NearDrop drop={drop} icon={icons.newDrop}/>
     } else {
         return <FarDrop drop={drop}/>
     }
