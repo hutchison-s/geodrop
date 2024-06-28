b9df0c6624444ea34f3a65ae9f670bb80f826f36
Steven Hutchison <hutchison.music@gmail.com>
2024-06-28 17:21:47 -0500

    Provide feedback that no location is being shared or has been found in a reusable component

---
+++ react/src/components/NoLocation.jsx
@@ -0,0 +1,10 @@
+export default function NoLocation() {
+
+    return (
+        <>
+            <div id="noLocation"><h2>Where are you?</h2><i className="fa-solid fa-location-crosshairs"></i><div className="textCenter padL">
+                <p>No location detected...</p><p>Please share your location to use the website</p>
+            </div></div>
+        </>
+    )
+}
