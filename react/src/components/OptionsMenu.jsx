d8269f23d4a4d0e58b8032781d0aa065694a3873
Steven Hutchison <hutchison.music@gmail.com>
2024-06-28 13:18:10 -0500

    Correct option menu styling for buttons

--- react/src/components/OptionsMenu.jsx
+++ react/src/components/OptionsMenu.jsx
@@ -1,11 +1,12 @@
 import { useMode } from "../contexts/LightContext";
 import { useUser } from "../contexts/UserContext"
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PropTypes from 'prop-types'
 
 export default function OptionsMenu({visible, close}) {
     const {logout} = useUser();
     const {mode, switchMode} = useMode();
+    const navigate = useNavigate();
 
     const nextMode = mode === 'light' ? "Dark" : "Light";
 
@@ -13,7 +14,7 @@ export default function OptionsMenu({visible, close}) {
         <ul id="optionsMenu" className={`flex vertical fixed ${visible ? 'visible' : ''}`}>
             
             <li><button onClick={logout}>Log Out</button></li>
-            <li><Link to='/about'>About GeoDrop</Link></li>
+            <li><button onClick={navigate('/about')}>About GeoDrop</button></li>
             <li><button onClick={()=>{
                 switchMode();
                 close();
