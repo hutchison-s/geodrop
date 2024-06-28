2d06e2a078daf738f3b585bff21cc0d5cc621eac
Steven Hutchison <hutchison.music@gmail.com>
2024-06-28 10:55:31 -0500

    Add custom 404 page

--- react/src/pages/NotFound.jsx
+++ react/src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
 export default function NotFound() {
     return (
-        <>
-            <h2>404</h2>
-            <p>Page not found</p>
-        </>
+        <section id="error404">
+            <h2><span>4</span><span>0</span><span>4</span></h2>
+            <p>This page doesn&apos;t exist.</p>
+            <p>Check the url and try again.</p>
+        </section>
     )
 }
